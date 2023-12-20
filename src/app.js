bac05792817c246cad17dd09173175d261a01d14
Maria Camila Ortega <148597026+mcamilaortega@users.noreply.github.com>
2023-12-20 18:27:27 +0000

    still working on it

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector(".card");
+  let num = document.querySelector(".number");
+  let suits = ["hearts", "spades", "diamonds", "clubs"];
+  let ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let randomnumber = array => {
+    return Math.floor(Math.random() * array.length);
+  };
+  card.classList.add(suits[randomnumber(suits)]);
+  num.innerHTML = ranks[randomnumber(ranks)];
 };
